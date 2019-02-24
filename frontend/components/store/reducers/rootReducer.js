87f48e6589d1c4fe156b015c215b0ddaed701bd9
Xin Zhang <xinzhang@Xins-MacBook-Pro.local>
2019-02-24 11:49:39 +1100

    add the keyboard sorts

--- frontend/components/store/reducers/rootReducer.js
+++ frontend/components/store/reducers/rootReducer.js
@@ -2,14 +2,21 @@ import * as actions from 'store/actions/sortActions';
 import sortHelper from 'utilities/sortHelper';
 import rows from 'Mockup/people.js'
 
-function setSortOrder(sorts, column) {
+//use addOrderType to decide how to make the sorts order
+// for single mouse click, addOrderType = 1,  make the order column primary
+// for keyboard selection mouse click, addOrderType = 2, make the order added in sequence.
+function setSortOrder(sorts, column, addOrderType = 1) {
   const idx = sorts.findIndex(x => x.name === column);
   const found = sorts[idx];
 
-  if (idx === -1) {
+  if (idx === -1 && addOrderType === 1) {
     // first time, make this primary.
     return [ {name: column, order: 'asc' }, ...sorts ];
-    
+
+  } else if (idx === -1 && addOrderType === 2) {
+      // first time, make this primary.
+      return [...sorts, {name: column, order: 'asc' } ];
+
   } else if (found.order === 'asc') {
     // click from the asc, goes to desc
     return [...sorts.slice(0, idx), 
@@ -20,6 +27,8 @@ function setSortOrder(sorts, column) {
     // the column should be removed.
     return [...sorts.slice(0, idx),       
       ...sorts.slice(idx + 1)];
+  } else {
+    return sorts;
   }
 }
 
@@ -29,14 +38,14 @@ function getSortOrder(sorts) {
     return (sort.order === 'desc') ? `-${sort.name}` : sort.name;
   })
 
-  console.log(ordersequence);
-  return ordersequence;
-  
+  return ordersequence; 
 }
 
+//the order ordinal status will be start, inprogress, stop, default stop
 const initalState = {
   rows: sortHelper(rows, 'name'),
   sorts: [{name:'name', order: 'asc'}],
+  order_ordinal_status: 'stop',
 }
 
 export default function rootReducer(state = initalState, action) { 
@@ -45,8 +54,34 @@ export default function rootReducer(state = initalState, action) {
       const sortorders = setSortOrder([...state.sorts], action.columnSortInfo);
 
       return {
-        rows: sortHelper([...state.rows], ...getSortOrder),
+        rows: sortHelper([...state.rows], ...(getSortOrder(sortorders))),
         sorts: [...sortorders]
+      }
+
+    case actions.ORDER_SORT_COLUMN:
+      let newState = {
+        rows: state.rows,  
+        order_ordinal_status: 'inprogress',
+      }
+      // only when it is starting, we add the sorts order
+      // the sorts will happen in the stop order ordinal action in one go.
+      if (state.order_ordinal_status === 'start') {                
+        newState.sorts = setSortOrder([], action.columnSortInfo, 2);
+      } else {
+        newState.sorts = setSortOrder(state.sorts, action.columnSortInfo, 2);
+      }
+      return newState;
+
+    case actions.START_ORDER_ORDINAL:
+      return {
+        ...state,
+        order_ordinal_status: action.order_ordinal_status,
+      }
+    case actions.STOP_ORDER_ORDINAL:
+      return {        
+        rows: sortHelper([...state.rows], ...getSortOrder),
+        sorts: state.sorts,
+        order_ordinal_status: 'stop'
       }
 
     default:
